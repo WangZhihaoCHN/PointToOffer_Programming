5c1cf9edf24059a16d1e99c9e24c3a7d91f03419
wangzhihao <wangzhihao.chn@gmail.com>
2017-06-28 10:25:43 +0800

    字符串的排列

--- Permutation.java
+++ Permutation.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 
+/*
+* 题目描述：
+* 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,
+* 则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+*
+* 输入描述：
+* 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+*
+* */
+
 public class Test {
     /*
     * 算法思路：
