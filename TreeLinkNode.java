db1fe006b51de756c9db4b82196559b57f4b0603
WangZhihao <wangzhihao.chn@gmail.com>
2018-03-27 17:38:20 +0800

    二叉树结点(包含父节点指针)

---
+++ TreeLinkNode.java
@@ -0,0 +1,14 @@
+
+/**
+ * Created by Wangzhihao on 2017/7/21.
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
